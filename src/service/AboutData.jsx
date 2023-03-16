ef075b6d0f065fc5e2f7e7635532b0277e2f471d
PA7Colin <pierreantoinecolin@outlook.fr>
2023-03-16 20:26:37 +0100

    16032023

--- src/service/AboutData.jsx
+++ src/service/AboutData.jsx
@@ -1,6 +1,6 @@
 export const about = {
     fiability: "Les logements poster sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, toutes les informations sont régulièrement vérifiées par nos équipes.",
     respect: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de notre plateforme.",
-    service: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. Hésitez pas à nous contacter.",
-    security: "La sécurité est la priorité de Kasa. Aussi bien pour les hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services."
+    service: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.",
+    security: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
 }
